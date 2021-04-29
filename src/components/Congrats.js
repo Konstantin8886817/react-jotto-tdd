66efb3671fed640354a65a06d5870c19ffa9a8d4
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-04-29 10:48:54 +0300

    added prop-types

---
+++ src/components/Congrats.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// receive the success state as a prop
+const Congrats = (props) => {
+            
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">Congratulations! You guessed the word!</span>
+            </div>
+        )
+    } else {
+        return (
+            <div data-test="component-congrats"></div>
+        )
+    }
+}
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+}
+
+export default Congrats;
