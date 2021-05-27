147bcdee55abb92d98fa9abdb7a73b04ee827150
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-05-27 16:02:06 +0300

    added functional tests

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 
 import Congrats from './components/Congrats';
 import GuessedWords from './components/GuessedWords';
+import Input from './components/Input';
 
 function App() {
+  // get props from shared state
+  const success = false;
+  const secretWord = 'party';
+  const guessedWords = [];
+
   return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
-      <Congrats success={true} />
+      <Congrats success={success} />
+      <Input success={success} secretWord={secretWord}/>
       <GuessedWords guessedWords={[ { guessedWord: 'train', letterMatchCount: 3 } ]} />
     </div>
   );
