96f5c5cb5e3d1cd4628d8ce4d4d7e4a34182bdd2
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-05-30 16:07:34 +0300

    added secret word reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import success from './successReduser';
 import guessedWords from './guessedWordsReducer';
+import secretWord from './guessedWordsReducer';
 
 export default combineReducers({
     success,
     guessedWords,
+    secretWord,
 });
