437174b178510ad639d6cca2f3d68fbc1eb80b99
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-05-30 15:30:41 +0300

    added guessed words reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import success from './successReduser';
+import guessedWords from './guessedWordsReducer';
 
 export default combineReducers({
     success,
+    guessedWords,
 });
